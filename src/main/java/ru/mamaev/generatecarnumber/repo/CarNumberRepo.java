23c9103b2cebbbfc2fc3ada1085f2f44d50530c4
Pavel Mamaev <mamai_88@mail.ru>
2021-04-02 13:29:00 +0300

    Добавил интерфейс для работы с БД

--- src/main/java/ru/mamaev/generatecarnumber/repo/CarNumberRepo.java
+++ src/main/java/ru/mamaev/generatecarnumber/repo/CarNumberRepo.java
@@ -3,5 +3,5 @@ package ru.mamaev.generatecarnumber.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.mamaev.generatecarnumber.pojo.CarNumber;
 
-public interface CarNumberRepo extends JpaRepository<CarNumber,Long> {
+public interface CarNumberRepo extends JpaRepository<CarNumber, Long> {
 }
